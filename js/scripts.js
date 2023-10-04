1af22bc8ef234816ba50c35652b15a72c3d7dc8b
Eric Fu <eric.fu@mercury.eco>
2023-10-03 19:33:51 -0600

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,31 @@
+const API_URL = 'http://localhost:8080/contacts';
+
+$(document).ready(function () {
+  $.ajax({
+    url: `${API_URL}`
+  }).then(function (data) {
+    let tableRows;
+
+    data.forEach((d) => {
+      tableRows += `
+      <tr>
+        <td>${d.id}</td>
+        <td>${d.first_name}</td>
+        <td>${d.last_name}</td>
+        <td>${d.email}</td>
+        <td>+${d.country_code} ${d.phone}</td>
+        <td>${d.notes}</td>
+        <th><a class="waves-effect waves-light btn">Edit</a></th>
+      </tr>
+    `;
+    });
+
+    $('.contacts tbody').append(tableRows);
+  });
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+  var elems = document.querySelectorAll('select');
+  var options = {};
+  var instances = M.FormSelect.init(elems, options);
+});

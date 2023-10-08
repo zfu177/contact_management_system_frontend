8adb0b80df21761876d28ff99dbdb4e9864e97b7
Eric Fu <eric.fu@mercury.eco>
2023-10-07 19:47:15 -0600

    add addContact function

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,7 @@ const API_URL = 'http://localhost:8080/contacts';
 
 $(document).ready(function () {
   $.ajax({
-    url: `${API_URL}`
+    url: API_URL
   }).then(function (data) {
     let tableRows;
 
@@ -31,3 +31,38 @@ document.addEventListener('DOMContentLoaded', function () {
   var options = {};
   var instances = M.FormSelect.init(elems, options);
 });
+
+function add_contact() {
+  event.preventDefault();
+  const first_name = document.getElementById('first_name').value;
+  const last_name = document.getElementById('last_name').value;
+  const email = document.getElementById('email').value;
+  const country_code = document.getElementById('country_code').value;
+  const phone = document.getElementById('phone').value;
+  const notes = document.getElementById('notes').value;
+
+  console.log({
+    firstName: first_name,
+    lastName: last_name,
+    email,
+    countryCode: parseInt(country_code),
+    phone: parseInt(phone),
+    notes
+  });
+
+  $.post(API_URL, {
+    firstName: first_name,
+    lastName: last_name,
+    email,
+    countryCode: 1,
+    phone: 212341234123,
+    notes
+  })
+    .fail(function (data) {
+      alert(JSON.stringify(data.responseText, null, 2));
+    })
+    .done(function (data) {
+      alert('Success');
+      window.open('index.html');
+    });
+}
